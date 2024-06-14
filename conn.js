f57218b828a52df690daaa27b0da0471187b15db
Tahacollege <tcollegewala30@gmail.com>
2024-06-14 23:13:13 +0530

    connection

--- conn.js
+++ conn.js
@@ -1,6 +1,6 @@
 const {MongoClient}=require('mongodb')
 const url='mongodb://0.0.0.0:27017'
-const client=new MongoClient(url)
+const client=new MongoClient(process.env.MONGO_URI)
 async function connection(){
     var result=await client.connect()
     var db=result.db('Bags')
